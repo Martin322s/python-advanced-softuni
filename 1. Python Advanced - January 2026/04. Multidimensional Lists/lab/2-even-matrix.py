af2fb7ae46a031dd9a871fe5b6ae31ab8a5bfa6c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-03-01 12:58:36 +0200

    even matrix elements py file

---
+++ 1. Python Advanced - January 2026/04. Multidimensional Lists/lab/2-even-matrix.py
@@ -0,0 +1,7 @@
+rows = int(input())
+
+matrix = [[int(x) for x in input().split(", ") if int(x) % 2 == 0] for _ in range(rows)]
+
+print(matrix)
+
+# print([[int(x) for x in input().split(", ") if int(x) % 2 == 0] for _ in range(int(input()))])
