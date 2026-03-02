ccda8e64d045598dc5377ec90b3e82557a275866
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-03-02 12:59:25 +0200

    primary diagonal py file

---
+++ 1. Python Advanced - January 2026/04. Multidimensional Lists/lab/5-primary-diagonal.py
@@ -0,0 +1,13 @@
+matrix_size = int(input())
+matrix = []
+
+for _ in range(matrix_size):
+    row = [int(x) for x in input().split(" ")]
+    matrix.append(row)
+
+primary_diagonal_sum = 0
+
+for i in range(matrix_size):
+    primary_diagonal_sum += matrix[i][i]
+
+print(primary_diagonal_sum)
