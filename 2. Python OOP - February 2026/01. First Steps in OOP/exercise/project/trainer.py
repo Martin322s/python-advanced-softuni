07f5c8f749ad38c81771f6410597a99c9117454a
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-03-23 18:17:03 +0200

    trainer py

---
+++ 2. Python OOP - February 2026/01. First Steps in OOP/exercise/project/trainer.py
@@ -0,0 +1,29 @@
+from project.pokemon import Pokemon
+
+class Trainer:
+    def __init__(self, name: str):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if any(p.name == pokemon.name for p in self.pokemons):
+            return "This pokemon is already caught"
+
+        self.pokemons.append(pokemon)
+        return f"Caught {pokemon.pokemon_details()}"
+
+    def release_pokemon(self, pokemon_name: str):
+        for pokemon in self.pokemons:
+            if pokemon.name == pokemon_name:
+                self.pokemons.remove(pokemon)
+                return f"You have released {pokemon_name}"
+        return "Pokemon is not caught"
+
+    def trainer_data(self):
+        result = f"Pokemon Trainer {self.name}\n"
+        result += f"Pokemon count {len(self.pokemons)}"
+
+        for pokemon in self.pokemons:
+            result += f"\n- {pokemon.pokemon_details()}"
+
+        return result
