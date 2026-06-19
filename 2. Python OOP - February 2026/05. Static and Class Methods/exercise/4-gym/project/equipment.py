2c546e043cb5a7059a0f9017aa2f94eda38c4eef
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-06-19 15:53:55 +0300

    Add Equipment class for managing gym equipment details

---
+++ 2. Python OOP - February 2026/05. Static and Class Methods/exercise/4-gym/project/equipment.py
@@ -0,0 +1,14 @@
+class Equipment:
+    EQUIPMENT_ID = 1
+    def __init__(self, name):
+        self.name = name
+        self.id = self.get_next_id()
+
+    @staticmethod
+    def get_next_id():
+        id = Equipment.EQUIPMENT_ID
+        Equipment.EQUIPMENT_ID += 1
+        return id
+    
+    def __repr__(self):
+        return f"Equipment <{self.id}> {self.name}"
